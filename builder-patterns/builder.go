128ec12689432b5f29ef11cffb35e94dd587ec72
lee <yeluowoxin@sina.com>
2019-08-27 20:22:18 +0800

    builder patterns

---
+++ builder-patterns/builder.go
@@ -0,0 +1,60 @@
+package builder
+
+/*
+	separate the construction of a complex object from representation
+	核心模块：
+		*Builder interface (包含1.biuld_XX method 返回的是biulder接口，2.get_XX 返回对象)
+		*父struct
+		*Director struct, 属性为Builder, 实现Construct()和SetBuilder()方法
+		*不同的子struct组合，实现接口builder
+*/
+type Wheel int
+//定义父struct
+type Vehicle struct {
+	Wheels	  Wheel
+	Seats 	  int
+	Structure string
+}
+
+//builder interface
+type Builder interface {
+	SetWheels()		Builder
+	SetSeats()		Builder
+	SetStructure()  Builder
+	GetVehicle()	Vehicle
+}
+
+//Director
+type Director struct {
+	builder Builder
+}
+
+func (director *Director) Construct() {
+	director.builder.SetWheels().SetSeats().SetStructure()  //链式调用
+}
+
+func (director *Director) SetBuilder(builder Builder) {
+	director.builder = builder
+}
+
+//car struct
+type Car struct {
+	vehicle Vehicle
+}
+
+//实现继承Builder
+func (car *Car) SetWheels() Builder {
+	car.vehicle.Wheels = 4
+	return car
+}
+func (car *Car) SetSeats() Builder {
+	car.vehicle.Seats = 4
+	return car
+}
+func (car *Car) SetStructure() Builder {
+	car.vehicle.Structure = "Car"
+	return car
+}
+func (car *Car) GetVehicle() Vehicle {
+	return car.vehicle
+}
---
+++ builder-patterns/builder_test.go
@@ -0,0 +1,27 @@
+package builder
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestBuilderPattern(t *testing.T) {
+	director := Director{}
+
+	//car
+	car := &Car{}
+	director.SetBuilder(car)
+	director.Construct()
+	vehicle := director.builder.GetVehicle()
+	//vehicle = car.GetVehicle()
+	fmt.Println(vehicle)
+	if vehicle.Wheels != 4 {
+		t.Errorf("car wheels must be 4, but get %d\n", vehicle.Wheels)
+	}
+	if vehicle.Wheels != 4 {
+		t.Errorf("car seats must be 4, but get %d\n", vehicle.Seats)
+	}
+	if vehicle.Wheels != 4 {
+		t.Errorf("vehicle structure must be Car, but get %s\n", vehicle.Structure)
+	}
+}
